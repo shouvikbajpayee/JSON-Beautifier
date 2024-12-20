0c62f23ab82a7d2a37aef7193ddfb59ac2cfed77
Shouvik Bajpayee <shouvikbajpayee@CKMAC283.local>
2024-12-20 14:24:16 +0530

    Added author information

--- src/components/JsonViewer.jsx
+++ src/components/JsonViewer.jsx
@@ -4,25 +4,27 @@ import ReactJson from 'react-json-view';
 const JsonViewer = ({ jsonData }) => {
     return (
         <>
-            <h1>Beautified JSON Viewer</h1>
-            <div className='container'>
-                {jsonData ? (
-                    <ReactJson
-                        src={jsonData}
-                        theme="tomorrow"
-                        enableClipboard={true}
-                        style={{
-                            padding: "20px",
-                            borderRadius: "20px",
-                            width: "90%"
-                        }}
-                        iconStyle='circle'
-                        indentWidth={6}
-                    />
-                ) : (
-                    <p>No JSON data to display</p>
-                )}
-            </div>
+            {jsonData ? (
+                <>
+                    <h1>Beautified JSON</h1>
+                    <div className='container'>
+                        <ReactJson
+                            src={jsonData}
+                            theme="tomorrow"
+                            enableClipboard={true}
+                            style={{
+                                padding: "20px",
+                                borderRadius: "20px",
+                                width: "90%"
+                            }}
+                            iconStyle='circle'
+                            indentWidth={6}
+                        />
+                    </div>
+                </>
+            ) : (
+                <p></p>
+            )}
         </>
     );
 };
